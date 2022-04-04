eae4b85552e835dc364098ae15b1625e655a34d4
James Jones <atari@theinnocuous.com>
2022-04-03 22:45:05 -0700

    Implement a Skunkboard 2+ flash driver
    -Replace the AMD flash commands with those used
     by the the AT49BV642D chip used on the Skunkboard
    
    -Use an offset into ROM1 space for the 'NVRAM'
     base address, since the Skunkboard BIOS and the
     manager program occupy the bottom of the ROM
     space on the Skunkboard, unlike the fancy
     ROMWIDTH tricks & interleaving used on the real
     Memory Track.
    
    -Add code to toggle the ROMWIDTH/ROMSPEED fields
     of MEMCON1 when needed to access the Skunkboard
     as a 16-bit cartridge.
    
    -Add code to put the Skunkboard into flash
     read+write mode before sending write and erase
     commands to it.

--- nvmskunk.s
+++ nvmskunk.s
@@ -4,7 +4,7 @@
 ; All Rights Reserved
 ; Version 1.01
 ;
-; AMD29F010 version
+; Skunkboard version
 ;
 ; This is the 2K module that gets loaded from
 ; the cartridge ROM into Jaguar RAM. The main
@@ -13,9 +13,57 @@
 ;
 
 	.include	"asmnvm.s"
-_NVRAM	=	$800000
+_CART	=	$800000
+; Third 64k sector of flash is start of application-accessible NVRAM
+_NVRAM	=	(_CART+$22000)
+
+
+; The NVRAM API only gives the NVRAM BIOS code a big enough scratch buffer to
+; handle 16k sectors. The Skunkboard 2+ flash chip has 64k sectors (4 * 16k).
+; However, the original NVRAM BIOS code also uses some funky logic to access the
+; 8-bit NVRAM while in 32-bit ROM mode, and ends up multiplying every address by
+; 4 and accessing each byte as a DWORD on the bus. These factors cancel each-
+; other out. The 4x addressing logic from the original BIOS code is preserved,
+; and the result is the sector sizes line up and 3/4 bytes just get wasted,
+; meaning emulating the original 128k memory size uses 512k of flash memory on
+; the Skunkboard.
 SECTORSIZE   = 16384
 LOG2_SECTORSIZE = 14
+
+; setrom: Set ROMWIDTH = 16bit, ROMSPEED = 5 cycles
+; Parameters: none
+; Internal register usage:
+; d0 = scratch
+; d3.w = prior MEMCON1 value
+; a4 = MEMCON1 address
+;
+; Registers clobbered:
+; d0, d3, a4
+setrom:
+	; MEMCON1 = $F00000
+	lea	$F00000,a4
+	move.w	(a4),d0				; Load current MEMCON1 value
+	move.l	d0,d3				; Get a copy
+	and.w	#$ffe1,d0			; Mask off (ROMSPEED|ROMWIDTH)
+	or.w	#$1a,d0				; Set 16-bit 5 cycle width/speed
+	move.w	d0,(a4)				; Set it.
+	nop					; Let it settle.
+	nop
+	rts
+
+; restorerom: Set ROMWIDTH = 16bit, ROMSPEED = 5 cycles
+; Parameters:
+; a4 = MEMCON1 address
+; d3.w = desired MEMCON1 value
+;
+; Internal register usage: a4, d3 as defined above.
+;
+; Preserves all registers
+restorerom:
+	move.w	d3,(a4)				; Restore MEMCON1 from d3
+	nop					; Let it settle
+	nop
+	rts
 
 ;
 ;****************************************************************
@@ -39,9 +87,12 @@ LOG2_SECTORSIZE = 14
 ; Internal register usage:
 ; a0 = source address
 ; a1 = dest. address
-; a2 = NVRAM base address
-; a3 = NVRAM+(4*($5555))
-; a4 = unused, available
+; a2 = ROM1 base address
+; a3 = ROM1+(4*($5555))
+; a4 = MEMCON1 address
+; d3 = prior MEMCON1 value
+; d4 = sector address << 2
+; d5 = SECTORSIZE loop iterator
 ; d6 = master retry count for whole flush
 ; d7 = retry count for individual write operations
 ;
@@ -52,8 +103,8 @@ LOG2_SECTORSIZE = 14
 ; utility function: sends the command prefix to the ROM
 ;
 sendcmd:
-	move.w	#$00AA, (a3)
-	move.w	#$0055, _NVRAM+(4*$2aaa)
+	move.w	#$9098, (a3)			; special command
+	move.w	#$C501, _CART+($1C94)
 	rts
 FLUSH:
 	movem.l	d0-d7/a0-a4, -(sp)
@@ -62,7 +113,10 @@ FLUSH:
 	beq	L2				; if cache is empty, return
 	moveq	#3,d6				; initialize master retry count
 	lea	_NVRAM,a2
-	lea	_NVRAM+(4*$5555),a3
+	lea	_CART+($36A),a3
+
+	bsr	setrom				; Configure MEMCON1 for skunk
+	move.w	#$4000, $C00000			; Put skunk in read+write mode
 
 RETRYFLUSH:
 	subq.w	#1,d6				; retries exhausted yet?
@@ -78,10 +132,10 @@ RETRYFLUSH:
 ; send the "erase sector" command
 ;
 	bsr	sendcmd
-	move.w	#$0080, (a3)			; _NVRAM+(4*($5555))
+	move.w	#$8008, (a3)			; $0080
 
 	bsr	sendcmd
-	move.w	#$0030, (a1)
+	move.w	#$8480, (a1)			; $0030
 	
 ;
 ; wait for sector erase to complete
@@ -90,12 +144,12 @@ RETRYFLUSH:
 	nop
 	nop
 .loop:
-	move.l	(a1),d0
-	btst	#(7+16),d0			; finished yet?
+	move.w	(a1), d0
+	and.w	#8, d0				; finished yet?
 	bne.b	.done				; yes: we're done
-	btst	#(5+16),d0			; timeout error?
-	beq.b	.loop				; no -- keep looping
-	bra	RETRYFLUSH			; yes bad news, give up
+	;btst	#(5+16),d0			; XXX Handle timeout error
+	bra.b	.loop				; keep looping
+	;bra	RETRYFLUSH			; XXX timeout error, give up
 						; (technically we should check bit 7 again)
 .done:
 
@@ -105,30 +159,32 @@ L6:
 RETRYWRITE:
 	move.b	(a0), d0			; d0 = the byte to write
 	not.w	d0				; invert bytes, so that erased sectors ($FF) read as 0's
-	swap	d0				; put d0 in low byte of high word
+
+	; Skunk: Only going to write the low word of each dword. Keep the byte
+	; we care about in that word.
+	;swap	d0				; put d0 in low byte of high word
+
 ;
 ; write byte d0 to a1
 ;
-	move.l	d0,d1				; save what we wrote
+
 ;
 ; send the write command
 ;
 	bsr	sendcmd
-	move.w	#$00A0, (a3)			; _NVRAM+(4*($5555))
-	move.l	d0, (a1)
+	move.w	#$8088, (a3)			; $00A0
+	move.w	d0, (a1)
 ;
 ; now wait for it to finish
 ; use the data polling algorithm
 ;
 .loop:
-	move.l	(a1),d1
-	move.l	d1,d2				; save d1
-	eor.l	d0,d1
-	btst.l	#(7+16),d1			; did bit 7 match what we wrote?
-	beq.b	.done				; they match: we're done
-	btst.l	#(16+5),d2			; error indication?
+	move.w	(a1),d1
+	cmp.w	d1, d0				; Do the values match?
+	beq.b	.done				; yes: we're done
+	and.w	#$1080, d1			; error indication?
 	beq.b	.loop				; no: keep going
-	subq	#1,d7				; yes: check the retry count
+	subq	#1, d7				; yes: check the retry count
 	bpl	RETRYWRITE			; and retry if we should
 	bra	FAILWRITE			; yes: fail (we really should check again)
 .done:
@@ -136,10 +192,6 @@ RETRYWRITE:
 	addq.l	#1,a0
 	subq.w	#1,d5
 	bpl.b	L6
-
-	; reset the hardware for reading
-	bsr	sendcmd
-	move.w	#$00F0, (a3)			; _NVRAM+(4*($5555))
 
 	; now do a verify of the sector
 	moveq.l	#0,d4				; make sure high word of d4 is 0
@@ -162,6 +214,9 @@ RETRYWRITE:
 	bpl.b	.verifyloop
 
 Lreturn:					; return
+	move.w	#$4001, $C00000			; Put skunk in read-only mode
+	bsr	restorerom			; Restore prior MEMCON1 value
+
 	move.w	#-1, cache_sector(a5)			; mark cache as clear
 L2:
 	movem.l	(sp)+, d0-d7/a0-a4
@@ -172,9 +227,6 @@ L2:
 FAIL:
 	nop
 FAILWRITE:
-	; reset the hardware for reading
-	bsr	sendcmd
-	move.w	#$00F0, _NVRAM+(4*($5555))
 	bra.b	Lreturn
 
 ;
@@ -201,6 +253,9 @@ Getbyte:
 	move.b	0(a1,d1.l),d0
 	rts
 .notcached:
+	movem.l	d3/a4, -(sp)
+	bsr	setrom				; Configure MEMCON1 for skunk
+
 	move.l	a0,d1
 	add.l	d1,d1				; the actual byte offset in ROM is multiplied by 4
 	add.l	d1,d1
@@ -209,6 +264,9 @@ Getbyte:
 	swap	d0
 	not.w	d0				; invert bytes, so cleared sectors read as 0
 	and.l	#$000000ff,d0			; mask off other stuff
+
+	bsr	restorerom			; Restore prior MEMCON1 value
+	movem.l	(sp)+, d3/a4
 	rts
 
 ;
