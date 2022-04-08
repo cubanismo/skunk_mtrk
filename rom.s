4ba35954c3f925ea2cc87a40ade3d97c6026d8fd
James Jones <atari@theinnocuous.com>
2022-04-07 19:10:42 -0700

    Give the ROM a header the Skunkboard can recognize
    By necessity, the memory track BIOS ROM will be
    auto-booted when the user activates the CD boot
    mode by holding down 'C' while booting the
    Skunkboard in a Jaguar CD unit. To ensure the
    Skunkboard BIOS doesn't boot some random other
    ROM, give this one a unique signature the
    Skunkboard BIOS can check for, similar to the
    existing auto-boot code in bank 2, but which the
    BIOS will only look for when booting in CD mode.

--- rom.s
+++ rom.s
@@ -5,8 +5,20 @@ VI		=	$f0004e
 ; down to where it wants to be
 ;
 ; first, though, it checks to see if we're booting from the CD-ROM
-; boot rom; if not, it disables video (so that
+; boot rom; if not, it disables video (so that [sic]
 ;
+; Even before that though, branch over a marker the Skunkboard BIOS
+; uses to detect this ROM and auto-boot it when in CD boot mode.
+
+	bra.s	.realboot
+
+	.long
+	.dc.l	'SKNK'				; Identifiers
+	.dc.l	'MTRK'
+	.dc.l	$00802000			; Boot address
+	.dc.l	$00010000			; Version: 1.0.0
+
+.realboot:
 	move.l	$2400.w,d0			; check for boot rom magic
 	cmp.l	#$12345678,d0
 	beq.b	.cdboot
